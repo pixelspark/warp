ede9d0df7fdf4c44bff5eb20629cafef804d1890
Tommy van der Vorst <tommy@pixelspark.nl>
2014-11-01 15:14:09 +0100

    Initial Commit

---
+++ QBE/QBE.h
@@ -0,0 +1,19 @@
+//
+//  QBE.h
+//  QBE
+//
+//  Created by Tommy on 01-11-14.
+//  Copyright (c) 2014 Pixelspark. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for QBE.
+FOUNDATION_EXPORT double QBEVersionNumber;
+
+//! Project version string for QBE.
+FOUNDATION_EXPORT const unsigned char QBEVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QBE/PublicHeader.h>
+
+
