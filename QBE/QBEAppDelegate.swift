c887489e1ccecd35ba9c9a93240a5739c0e251b2
Tommy van der Vorst <tommy@pixelspark.nl>
2015-02-09 23:23:25 +0100

    Allow opening CSV files directly from the Finder

--- QBE/QBEAppDelegate.swift
+++ QBE/QBEAppDelegate.swift
@@ -9,5 +9,22 @@ class QBEAppDelegate: NSObject, NSApplicationDelegate {
 	func applicationWillTerminate(aNotification: NSNotification) {
 		// Insert code here to tear down your application
 	}
+	
+	func application(sender: NSApplication, openFile filename: String) -> Bool {
+		if let dc = NSDocumentController.sharedDocumentController() as? NSDocumentController {
+			if let u = NSURL(fileURLWithPath: filename) {
+				let doc = QBEDocument()
+				
+				/* TODO: check here what kind of file was opened; if it was a .csv, use QBECSVSourceStep, otherwise maybe
+				QBESQLiteSourceStep, etc. */
+				doc.head = QBECSVSourceStep(url: u)
+				dc.addDocument(doc)
+				doc.makeWindowControllers()
+				doc.showWindows()
+				return true
+			}
+		}
+		return false
+	}
 }
 
