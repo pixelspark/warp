f8e1da9b7965755d8857dad5c69bbf1a762eb91b
Tommy van der Vorst <tommy@pixelspark.nl>
2017-01-08 18:44:17 +0100

    Update licensing headers and copyright years

--- Warp/Sources/QBEObjectiveCBridge.h
+++ Warp/Sources/QBEObjectiveCBridge.h
@@ -1,4 +1,4 @@
-/* Warp. Copyright (C) 2014-2016 Pixelspark, Tommy van der Vorst
+/* Warp. Copyright (C) 2014-2017 Pixelspark, Tommy van der Vorst
  
  This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public 
  License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later 
